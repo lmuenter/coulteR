b213b1674237dab70ccb61ef5729c54bad4c0611
lmuenter <50193931+lmuenter@users.noreply.github.com>
2021-02-16 20:50:06 +0100

    added coulter-reader.R

---
+++ R/coulter-reader.R
@@ -0,0 +1,10 @@
+###############################
+#
+# Coulter Reader
+#
+###############################
+
+# purpose: A parser for Coulter's dirty AccuComp format
+# date: 16.02.2021
+# author: Lukas Muenter
+
